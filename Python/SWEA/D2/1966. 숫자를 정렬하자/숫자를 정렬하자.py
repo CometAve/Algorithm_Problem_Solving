07be27121b0ac4130418af7d694823b4c4340439
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-09 01:32:21 +0900

    [D2] Title: 숫자를 정렬하자, Time: 54 ms, Memory: 53,376 KB -BaekjoonHub

--- Python/SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.py
+++ Python/SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.py
@@ -1,10 +1,29 @@
 T = int(input())
 
 for test_case in range(1, T + 1):
-  # 주어진 숫자 개수
+
+  # 수의 개수 (5 <= N <= 50)
   N = int(input())
   
-  # 숫자열 받기
-  numbers = sorted(list(map(int, input().split())))
+  # 입력받은 수를 저장할 리스트
+  numbers = list(map(int, input().split()))
+
+  # 카운트 배열 생성
+  number_count = [0] * 51
   
-  print(f'#{test_case}', *numbers)
+  # 오름차순 정렬된 리스트
+  sorted_list = []
+
+  # 입력받은 수를 카운트 배열에 저장
+  for number in numbers:
+      number_count[number] += 1
+
+  # 카운트 배열을 순회하면서
+  # 0이 아닌 원소를 발견하면
+  # 해당 인덱스를 그 원소의 개수만큼 sorted_list에 추가
+  for i in range(0, 51):
+      if number_count[i] != 0:
+          for _ in range(number_count[i]):
+              sorted_list.append(i)
+  
+  print(f'#{test_case} {" ".join(map(str, sorted_list))}')
