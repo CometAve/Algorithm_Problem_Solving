e5ce914e871e4fd16423efa28c39833a971dad56
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-09 00:30:51 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 80 ms, Memory: 68,864 KB -BaekjoonHub

--- Python/SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ Python/SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -5,10 +5,7 @@ for test_case in range(1, 11):
     for i in range(2, N - 2):
         b_lst = b[i - 2:i + 3]
         c_b = b_lst.pop(2)
-        highest = 0
-        for h in b_lst:
-            if h > highest:
-                highest = h
+        highest = max(b_lst)
         if c_b > highest:
             v += (c_b - highest)
     print(f'#{test_case} {v}')
