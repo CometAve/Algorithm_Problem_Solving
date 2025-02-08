3d55f7f2770a73c280d4a56fff8e5daed082dfd9
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-09 00:30:25 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 92 ms, Memory: 72,960 KB -BaekjoonHub

--- Python/SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ Python/SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -2,10 +2,13 @@ for test_case in range(1, 11):
     N = int(input())
     b = list(map(int, input().split()))
     v = 0
-    b_lst = []
     for i in range(2, N - 2):
-        b_lst = [b[i-2], b[i-1], b[i],b[i+1], b[i+2]]
-        b_lst.sort(reverse=True)
-        if b[i] == b_lst[0]:
-            v += b[i] - b_lst[1]
+        b_lst = b[i - 2:i + 3]
+        c_b = b_lst.pop(2)
+        highest = 0
+        for h in b_lst:
+            if h > highest:
+                highest = h
+        if c_b > highest:
+            v += (c_b - highest)
     print(f'#{test_case} {v}')
