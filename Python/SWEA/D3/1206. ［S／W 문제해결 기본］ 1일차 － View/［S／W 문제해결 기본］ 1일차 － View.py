6011256a54cfb85a3266af208a0f68ad2dc84b65
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-09 00:28:45 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 88 ms, Memory: 69,760 KB -BaekjoonHub

--- Python/SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ Python/SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -1,11 +1,17 @@
 for test_case in range(1, 11):
     N = int(input())
-    building_heights = list(map(int, input().split()))
-     
+    building_list = list(map(int, input().split()))
     total_view = 0
-     
     for i in range(2, N - 2):
-        max_height = max(building_heights[i-2], building_heights[i-1], building_heights[i+1], building_heights[i+2])
-        if building_heights[i] > max_height:
-            total_view += building_heights[i] - max_height
+ 
+        building_heights_list = building_list[i - 2:i + 3]
+        building_heights_list.pop(2)
+ 
+        highest = 0
+        for height in building_heights_list:
+            if height > highest:
+                highest = height
+ 
+        if building_list[i] > highest:
+            total_view += (building_list[i] - highest)
     print(f'#{test_case} {total_view}')
