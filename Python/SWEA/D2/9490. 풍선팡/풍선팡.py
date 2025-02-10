918eab340e423d777d2c09f703ff4c8e2b5b26e5
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-10 23:46:21 +0900

    [D2] Title: 풍선팡, Time: 160 ms, Memory: 74,496 KB -BaekjoonHub

--- Python/SWEA/D2/9490. 풍선팡/풍선팡.py
+++ Python/SWEA/D2/9490. 풍선팡/풍선팡.py
@@ -1,24 +1,21 @@
-def boom():
-    mx = 0
-    dirs = ((1,0), (-1,0), (0,1), (0,-1))
-    
+def fast_solve():
+    max_sum = 0
     for i in range(N):
         for j in range(M):
-            power = arr[i][j]
-            current = power
+            dist = board[i][j]
+            current = dist
+            for k in range(1, dist + 1):
+                # 상하좌우를 한번에 처리
+                if i - k >= 0: current += board[i-k][j]
+                if i + k < N: current += board[i+k][j]
+                if j - k >= 0: current += board[i][j-k]
+                if j + k < M: current += board[i][j+k]
             
-            # 방향별로 한번에 계산
-            for dy, dx in dirs:
-                for p in range(1, power + 1):
-                    ny, nx = i + dy * p, j + dx * p
-                    if 0 <= ny < N and 0 <= nx < M:
-                        current += arr[ny][nx]
-            
-            mx = max(mx, current)
-    return mx
+            max_sum = max(max_sum, current)
+    return max_sum
 
 T = int(input())
 for test_case in range(1, T + 1):
     N, M = map(int, input().split())
-    arr = [list(map(int, input().split())) for _ in range(N)]
-    print(f'#{test_case} {boom()}')
+    board = [list(map(int, input().split())) for _ in range(N)]
+    print(f'#{test_case} {fast_solve()}')
