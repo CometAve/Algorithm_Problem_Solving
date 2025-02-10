292f64e508146d580f0c56f8dc437d551c639903
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-10 23:50:25 +0900

    [D2] Title: 풍선팡, Time: 159 ms, Memory: 74,496 KB -BaekjoonHub

--- Python/SWEA/D2/9490. 풍선팡/풍선팡.py
+++ Python/SWEA/D2/9490. 풍선팡/풍선팡.py
@@ -1,16 +1,20 @@
-def fast_solve():
+def solve():
     max_sum = 0
     for i in range(N):
         for j in range(M):
-            dist = board[i][j]
-            current = dist
-            for k in range(1, dist + 1):
-                # 상하좌우를 한번에 처리
+            power = board[i][j]
+            # 현재 위치값으로 초기화
+            current = power
+            
+            # 한 번의 순회로 상하좌우 처리
+            for k in range(1, power + 1):
+                # 상하
                 if i - k >= 0: current += board[i-k][j]
                 if i + k < N: current += board[i+k][j]
+                # 좌우
                 if j - k >= 0: current += board[i][j-k]
                 if j + k < M: current += board[i][j+k]
-            
+                    
             max_sum = max(max_sum, current)
     return max_sum
 
@@ -18,4 +22,4 @@ T = int(input())
 for test_case in range(1, T + 1):
     N, M = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(N)]
-    print(f'#{test_case} {fast_solve()}')
+    print(f'#{test_case} {solve()}')
