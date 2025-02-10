f5a2d2f4a36b0c72c6bcd20860a6174297130350
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-10 15:29:14 +0900

    [D3] Title: 특별한 정렬, Time: 60 ms, Memory: 49,792 KB -BaekjoonHub

--- Python/SWEA/D3/19646. 특별한 정렬/특별한 정렬.py
+++ Python/SWEA/D3/19646. 특별한 정렬/특별한 정렬.py
@@ -6,10 +6,16 @@ for test_case in range(1, T + 1):
     N = int(input())
  
     # 정수 리스트
-    numbers = list(map(int, input().split()))
+    count_list = [0] * 101
  
-    # 내림차순 정렬
-    numbers.sort(reverse=True)
+    # 입력과 동시에 카운팅 배열에 저장
+    for i in input().split():
+        count_list[int(i)] += 1
+ 
+    # 카운팅 배열을 이용하여 내림차순으로 정렬된 리스트 생성
+    numbers = []
+    for i in range(100, 0, -1):
+        numbers.extend([i] * count_list[i])
  
     # 한 개씩 번갈아가면서 출력
     for i in range(0, N, 2):
