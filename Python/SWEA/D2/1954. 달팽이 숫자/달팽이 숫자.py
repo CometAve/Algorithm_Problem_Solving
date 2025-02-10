ddc81847f2ee906f222725b4a91087b6e39b2bc7
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-10 15:49:25 +0900

    [D2] Title: 달팽이 숫자, Time: 57 ms, Memory: 54,400 KB -BaekjoonHub

--- Python/SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ Python/SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,32 +1,31 @@
 T = int(input())
- 
-for test_case in range(1, T + 1):
-    N = int(input())
- 
-    snail = [[0] * N for _ in range(N) for _ in range(N)]
- 
-    # 우 하 좌 상
-    d = 0
- 
-    # 우 = 0, 하 = 1, 좌 = 2, 상 = 3
-    dx = [0, 1, 0, -1]
+
+def create_snail(N):
+    snail = [[0] * N for _ in range(N)]
+
+    x, y = 0, 0 # 시작점
+    d = 0 # 현재 방향 우 :1, 하 :2, 좌 :3, 상 :4
+    dx = [0, 1, 0, -1] # 방향 벡터
     dy = [1, 0, -1, 0]
- 
-    # 시작점
-    x, y = 0, 0
- 
-    # 1부터 가로 N까지 숫자 채우기
+
+    # 1부터 N*N까지 숫자를 채워넣는다.
     for i in range(1, N * N + 1):
         snail[x][y] = i
-        nx = x + dx[d]
-        ny = y + dy[d]
-        if 0 <= nx < N and 0 <= ny < N and snail[nx][ny] == 0:
-            x, y = nx, ny
-        else:
+        nx, ny = x + dx[d], y + dy[d]
+
+        # 다음 위치가 범위를 벗어나거나 이미 숫자가 채워져 있으면 방향을 바꿔준다.
+        if not (0 <= nx < N and 0 <= ny < N) or snail[nx][ny]:
             d = (d + 1) % 4
-            x = x + dx[d]
-            y = y + dy[d]
- 
+            nx, ny = x + dx[d], y + dy[d]
+
+        x, y = nx, ny
+
+    return snail
+
+for test_case in range(1, T + 1):
+    N = int(input())
+    result = create_snail(N)
+
     print(f'#{test_case}')
-    for i in range(N):
-        print(*snail[i])
+    for i in result:
+        print(*i)
