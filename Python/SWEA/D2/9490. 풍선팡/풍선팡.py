eb229e2ccb38bd6a9d5fcf1044cb7408ce4f9c8b
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-10 23:36:08 +0900

    [D2] Title: 풍선팡, Time: 263 ms, Memory: 74,880 KB -BaekjoonHub

--- Python/SWEA/D2/9490. 풍선팡/풍선팡.py
+++ Python/SWEA/D2/9490. 풍선팡/풍선팡.py
@@ -1,29 +1,22 @@
 T = int(input())
 
 def get_max_flowers(i, j, N, M, balloons):
-  x, y = i, j
-  dx = [0, 1, 0, -1]
-  dy = [1, 0, -1, 0]
-  current_flowers = balloons[i][j]
-  total_flowers = current_flowers
+  power = balloons[i][j]
+  total = power
   
-  for i in range(4):
-    for flower in range(1, current_flowers+1):
-      nx, ny = x + dx[i] * flower, y + dy[i] * flower
-      if 0 <= nx < N and 0 <= ny < M:
-        total_flowers += balloons[nx][ny]
-  return total_flowers
+  for di, dj in [(0,1), (1,0), (0,-1), (-1,0)]:
+    for dist in range(1, power+1):
+      ni, nj = i + di * dist, j + dj * dist
+      if 0 <= ni < N and 0 <= nj < M:
+        total += balloons[ni][nj]
+  return total 
 
 for test_case in range(1, T + 1):
   N, M = map(int, input().split())
   balloons = [list(map(int, input().split())) for _ in range(N)]
-  sum_list = []
   max_sum = 0
   for i in range(N):
     for j in range(M):
-      sum_list.append(get_max_flowers(i, j, N, M, balloons))
+      max_sum = max(max_sum, get_max_flowers(i, j, N, M, balloons))
       
-  for sum in sum_list:
-    if sum > max_sum:
-      max_sum = sum
   print(f'#{test_case} {max_sum}')
