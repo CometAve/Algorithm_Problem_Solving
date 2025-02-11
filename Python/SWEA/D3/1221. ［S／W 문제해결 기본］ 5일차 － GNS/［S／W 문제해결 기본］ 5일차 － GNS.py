2cda9d66459df764eba2c9b48ca56f5faad14d94
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-11 15:21:10 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - GNS, Time: 96 ms, Memory: 67,456 KB -BaekjoonHub

--- Python/SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
+++ Python/SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
@@ -12,10 +12,10 @@ for test_case in range(T):
     N = N[1:]
     count_lst = [0] * 10
 
+    # 외계어 숫자를 실제 숫자로 변환하여 카운트
     for number in input().split():
         count_lst[string_dict[number]] += 1
 
     print(f'#{N}')
-    # 한 단어를 하나씩 띄어서 출력하면서 한 줄로 출력
     for i in range(10):
         print(f'{string_lst[i]} ' * count_lst[i], end='')
