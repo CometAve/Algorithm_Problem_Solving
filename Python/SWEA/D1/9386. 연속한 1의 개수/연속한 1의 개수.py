e9a711b47654e678d6cf9d883bfc131caaae4cab
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-11 15:17:25 +0900

    [D1] Title: 연속한 1의 개수, Time: 67 ms, Memory: 51,456 KB -BaekjoonHub

--- Python/SWEA/D1/9386. 연속한 1의 개수/연속한 1의 개수.py
+++ Python/SWEA/D1/9386. 연속한 1의 개수/연속한 1의 개수.py
@@ -1,8 +1,10 @@
 T = int(input())
  
 for test_case in range(1, T + 1):
+  # 수열의 길이
   N = int(input())
-
-  max_count = max(map(len, input().split('0')))
+   
+  # 입력 수열 0으로 split하여 가장 긴 '1'의 연속 횟수를 구한다.
+  max_count = max(map(len, map(str, input().split('0'))))
    
   print(f'#{test_case} {max_count}')
