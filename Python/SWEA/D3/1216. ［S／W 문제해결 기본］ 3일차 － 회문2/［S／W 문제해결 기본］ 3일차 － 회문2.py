e5e6900f1ec533e4d1dbd531469d7e67542fe00f
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-12 17:29:40 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 6,802 ms, Memory: 75,520 KB -BaekjoonHub

---
+++ Python/SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -0,0 +1,32 @@
+N = 100
+
+T = 10
+
+for test_case in range(1, T+1):
+    tc_number = int(input())
+
+    arr = [input() for _ in range(N)]
+    max_length = 0
+
+    for i in range(N):
+        for j in range(N):
+            for k in range(3, N):
+                if j + k > N:
+                    break
+                word = arr[i][j:j + k]
+                if word == word[::-1]:
+                    if len(word) > max_length:
+                        max_length = len(word)
+
+    for j in range(N):
+        for i in range(N):
+            for k in range(3, N):
+                if i + k > N:
+                    break
+                word = ''
+                for l in range(k):
+                    word += arr[i + l][j]
+                if word == word[::-1]:
+                    if len(word) > max_length:
+                        max_length = len(word)
+    print(f'#{tc_number} {max_length}')
