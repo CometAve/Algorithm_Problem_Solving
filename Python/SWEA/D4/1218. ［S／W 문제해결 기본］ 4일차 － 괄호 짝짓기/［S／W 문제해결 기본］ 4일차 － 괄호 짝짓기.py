00e2f1b26768fafd4493bf572297757673da7964
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-13 15:52:36 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 61 ms, Memory: 51,712 KB -BaekjoonHub

--- Python/SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.py
+++ Python/SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.py
@@ -1,18 +1,21 @@
 T = 10
 
-def is_valid_bracket(brackets):
-    brackets = list(brackets)
-    type_of_brackets = ['(', ')', '[', ']', '{', '}', '<', '>']
-    count_list = [0] * 8
+# stack으로 풀어보기
+def remove_if_bracket_matched(string):
+    bracket_pairs = {'(': ')', '{': '}', '[': ']', '<': '>'}
+    stack = []
+    brackets = list(string)
     for bracket in brackets:
-        count_list[type_of_brackets.index(bracket)] += 1
-
-    if count_list[0] != count_list[1] or count_list[2] != count_list[3] or count_list[4] != count_list[5] or count_list[6] != count_list[7]:
-        return 0
-    else:
-        return 1
+        stack.append(bracket)
+        if len(stack) > 1:
+            if stack[-2] in bracket_pairs.keys() and stack[-1] == bracket_pairs[stack[-2]]:
+                stack.pop()
+                stack.pop()
+            else:
+                continue
+    return 1 if len(stack) == 0 else 0
 
 for tc in range(1, T + 1):
     N = int(input())
     brackets = input()
-    print(f'#{tc} {is_valid_bracket(brackets)}')
+    print(f'#{tc} {remove_if_bracket_matched(brackets)}')
