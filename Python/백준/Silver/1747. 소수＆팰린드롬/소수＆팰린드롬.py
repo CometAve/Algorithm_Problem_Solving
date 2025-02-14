c10b53d20720b2e9ac0f371c6cd8bc0873dc08df
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-14 23:54:59 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 164 ms, Memory: 32544 KB -BaekjoonHub

--- Python/백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
+++ Python/백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -1,6 +1,10 @@
 import sys
 input = sys.stdin.readline
 
+def is_palindrome(num):
+    s = str(num)
+    return s == s[::-1]
+
 def is_prime(n):
     if n < 2:
         return False
@@ -8,20 +12,39 @@ def is_prime(n):
         return True
     if n % 2 == 0:
         return False
-    i = 3
-    while i * i <= n:
-        if n % i == 0:
+    p = 3
+    while p * p <= n:
+        if n % p == 0:
             return False
-        i += 2
+        p += 2
     return True
 
 N = int(input())
 
-for i in range(N, 1003002):
-    s = str(i)
-    # 회문이 아니면 소수검사 안함
-    if s != s[::-1]:
-        continue
-    if is_prime(i):
+# 작은 경우 별도 처리
+if N <= 2:
+    print(2)
+    sys.exit(0)
+if N <= 3:
+    print(3)
+    sys.exit(0)
+if N <= 5:
+    print(5)
+    sys.exit(0)
+if N <= 7:
+    print(7)
+    sys.exit(0)
+if N <= 11:
+    print(11)
+    sys.exit(0)
+
+# N이 짝수면 다음 홀수부터 시작
+i = N
+if i % 2 == 0:
+    i += 1
+
+while True:
+    if is_palindrome(i) and is_prime(i):
         print(i)
         break
+    i += 2  # 짝수는 건너
