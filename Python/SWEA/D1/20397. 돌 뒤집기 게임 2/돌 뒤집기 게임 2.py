2dfe82b87fa88776805632613ee85ea0b19064d0
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-15 15:29:45 +0900

    [D1] Title: 돌 뒤집기 게임 2, Time: 57 ms, Memory: 51,968 KB -BaekjoonHub

--- Python/SWEA/D1/20397. 돌 뒤집기 게임 2/돌 뒤집기 게임 2.py
+++ Python/SWEA/D1/20397. 돌 뒤집기 게임 2/돌 뒤집기 게임 2.py
@@ -1,27 +1,14 @@
 T = int(input())
-            
-for tc in range(1, T+1):
-    # N: 돌의 개수, M: 뒤집기 횟수
-    N, M = map(int, input().split())
 
-    # 돌의 초기 상태
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())
     stones = list(map(int, input().split()))
 
     for _ in range(M):
-        # i번째 돌을 사이에 두고 마주보는 j개의 돌의 색을 확인 후 뒤집는다.
         i, j = map(int, input().split())
-        i -= 1 # i번째 돌 -> i-1 인덱스
-        for k in range(1, j+1): # k i번째 돌에서의 거리
-            if 0 <= i-k and i+k < N:
-                # 같은 색이면 뒤집고, 다른 색이면 그대로
-                if stones[i-k] == stones[i+k]:
-                    # if stones[i-k]:   # if stones[i-k] == 1:
-                    #     stones[i-k] = 0
-                    #     stones[i+k] = 0
-                    # else:             # stones[i-k] == 0
-                    #     stones[i-k] = 1
-                    #     stones[i+k] = 1
-                    stones[i-k] = (stones[i-k]+1)%2  # stones[i-k] ^= 1
-                    stones[i+k] = (stones[i+k]+1)%2
-    
+        for x in range(1, j+1):
+            if i-x-1 >= 0 and i+x-1 < len(stones) and stones[i-x-1] == stones[i+x-1]:
+                stones[i-x-1] = (stones[i-x-1]+1) % 2
+                stones[i+x-1] = (stones[i+x-1]+1) % 2
+
     print(f'#{tc}', *stones)
