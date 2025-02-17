b2dabc78d36739996812626b750d0d9207d85ccc
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-17 16:47:32 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기1, Time: 57 ms, Memory: 51,456 KB -BaekjoonHub

--- Python/SWEA/D4/1222. ［S／W 문제해결 기본］ 6일차 － 계산기1/［S／W 문제해결 기본］ 6일차 － 계산기1.py
+++ Python/SWEA/D4/1222. ［S／W 문제해결 기본］ 6일차 － 계산기1/［S／W 문제해결 기본］ 6일차 － 계산기1.py
@@ -1,6 +1,30 @@
 T = 10
 
+# 중위표기식 -> 후위표기식
 for tc in range(1, T+1):
   N = int(input())
-  formula = list(map(int, input().split('+')))
-  print(f'#{tc} {sum(formula)}')
+  formula = input()
+  stack = []
+  operator_stack = []
+  calculator = []
+  result = 0
+    
+  for i in range(N):
+    if formula[i].isdigit():
+      stack.append(formula[i])
+    elif formula[i] == '+':
+      operator_stack.append(formula[i])
+  else:
+    while operator_stack:
+      stack.append(operator_stack.pop())
+    
+    for i in range(N):
+      if stack[i].isdigit():
+        calculator.append(int(stack[i]))
+      elif stack[i] == '+':
+        if len(calculator) >= 2:
+          a = calculator.pop()
+          b = calculator.pop()
+          calculator.append(a + b)
+    result = calculator[0]  
+  print(f'#{tc} {result}')
