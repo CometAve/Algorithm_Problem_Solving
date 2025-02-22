a8a8ea76396a0a76d335b3ace9360d9d34b71f40
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-23 00:46:56 +0900

    [Silver II] Title: 마인크래프트, Time: 480 ms, Memory: 121996 KB -BaekjoonHub

--- PyPy3/백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ PyPy3/백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -3,26 +3,29 @@ input = sys.stdin.readline
 
 # N: 세로, M: 가로, B: 인벤토리 속 블록 수
 N, M, B = map(int, input().split())
-# N x M 배열로 배치된 블록들
-blocks = [list(map(int, input().split())) for _ in range(N)]
+# N x M 배열로 배치된 블록들 한 배열로 저장하려고 함
+ground = []
+for _ in range(N):
+  ground.extend(map(int, input().split()))
 
 min_time = 1e9
 max_height = 0
-for h in range(257):
-    time = 0
-    block = B
-    for i in range(N):
-        for j in range(M):
-            if blocks[i][j] < h:
-                time += h - blocks[i][j]
-                block -= h - blocks[i][j]
-            else:
-                time += 2 * (blocks[i][j] - h)
-                block += blocks[i][j] - h
-    if block < 0:
-        continue
-    if time <= min_time:
-        min_time = time
-        max_height = h
+max_ground = max(ground)
+min_ground = min(ground)
+
+for height in range(min_ground, max_ground+1):
+    add = 0
+    remove = 0
+    now_time = 0
+    for block in ground:
+        if block > height:
+            remove += block - height
+        else:
+            add += height - block
+    now_time = remove * 2 + add
+    if remove + B >= add:
+      if now_time <= min_time:
+          min_time = now_time
+          max_height = height
 
 print(min_time, max_height)
