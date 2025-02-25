6b81f1d0075b92a3169005094800777a854291cd
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-02-26 03:01:01 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+char = [list(map(str, input().strip())) for _ in range(5)]
+max_length = 0
+# 가장 긴 문자열 리스트 길이 찾기
+for i in range(5):
+    if len(char[i]) >= max_length:
+        max_length = len(char[i])
+
+# 가장 긴 문자열 리스트보다 길이가 짧은 문자열 리스트에 0 추가
+for i in range(5):
+    if len(char[i]) < max_length:
+        difference = max_length - len(char[i])
+        for j in range(difference):
+            char[i].append(0)
+
+# 이차원 배열에 저장한 문자열 리스트를 가로로 순회하면서
+for j in range(max_length):
+    for i in range(5):
+        # 원소가 0이면 출력하지 않고, 0이 아니면 출력
+        if char[i][j] == 0:
+            continue
+        else:
+            print(char[i][j], end='')
