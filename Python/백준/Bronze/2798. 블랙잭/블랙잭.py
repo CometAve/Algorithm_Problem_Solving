da7fe4abaa2dec3e64f012d508804a0358100335
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-03-03 20:20:00 +0900

    [Bronze II] Title: 블랙잭, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/2798. 블랙잭/블랙잭.py
+++ Python/백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,14 +1,17 @@
+# combination 사용하지 않은 버전
+
 import sys
 input = sys.stdin.readline
-from itertools import combinations
 
 N, M = map(int, input().split())
 arr = list(map(int, input().split()))
 max_sum = 0
 
-for i in combinations(arr, 3):
-    if max_sum < sum(i) <= M:
-        current_sum = sum(i)
-        max_sum = sum(i)
+for i in range(N):
+    for j in range(i+1, N):
+        for k in range(j+1, N):
+            current_sum = arr[i] + arr[j] + arr[k]
+            if max_sum < current_sum <= M:
+                max_sum = current_sum
 
 print(max_sum)
