3720ebeaad3db2f0f7e1838bb885e8a04e5fa8b4
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-03-05 13:56:50 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 69 ms, Memory: 53,632 KB -BaekjoonHub

--- Python/SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
+++ Python/SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
@@ -1,31 +1,21 @@
-def in_order(node, tree_dict, max_node, result):
-    if node > max_node:
-        return
-    
-    # 왼쪽 자식 노드가 있으면 방문
-    if 2*node <= max_node:
-        in_order(node*2, tree_dict, max_node, result)
-    
-    # 현재 노드의 값 추가
-    result.append(tree_dict[node])
-    
-    # 오른쪽 자식 노드가 있으면 방문
-    if 2*node+1 <= max_node:
-        in_order(node*2+1, tree_dict, max_node, result)
-
+def in_order(node):
+    global word
+    # node가 N보다 작거나 같을 때만 실행
+    if node <= N:
+        # 왼쪽 자식 노드 방문
+        in_order(node * 2)
+        # 단어 추가
+        word += tree[node - 1][1]
+        # 오른쪽 자식 노드 방문
+        in_order(node * 2 + 1)
+ 
 for tc in range(1, 11):
     N = int(input())
-    
-    # 딕셔너리로 트리 저장
-    tree_dict = {}
-    for i in range(1, N+1):
-        info = input().split()
-        tree_dict[int(info[0])] = info[1]
-    
-    # 단어 저장할 리스트
-    result = []
-    
-    # 중위 순회
-    in_order(1, tree_dict, N, result)
-    
-    print(f'#{tc} {"".join(result)}')
+    # 1. 트리 생성
+    tree = [input().split() for _ in range(N)]
+    # 단어 저장할 변수
+    word = ''
+    # 2. 중위 순회
+    in_order(1)
+ 
+    print(f'#{tc} {word}')
