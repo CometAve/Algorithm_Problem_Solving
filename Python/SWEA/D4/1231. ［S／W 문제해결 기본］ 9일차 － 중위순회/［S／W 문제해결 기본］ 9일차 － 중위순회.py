833d69309fb9f60a9490c582dceace6550162283
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-03-05 13:54:02 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 62 ms, Memory: 53,760 KB -BaekjoonHub

---
+++ Python/SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
@@ -0,0 +1,21 @@
+def in_order(node):
+    global word
+    # node가 N보다 작거나 같을 때만 실행
+    if node <= N:
+        # 왼쪽 자식 노드 방문
+        in_order(node * 2)
+        # 단어 추가
+        word += tree[node - 1][1]
+        # 오른쪽 자식 노드 방문
+        in_order(node * 2 + 1)
+
+for tc in range(1, 11):
+    N = int(input())
+    # 1. 트리 생성
+    tree = [input().split() for _ in range(N)]
+    # 단어 저장할 변수
+    word = ''
+    # 2. 중위 순회
+    in_order(1)
+
+    print(f'#{tc} {word}')
