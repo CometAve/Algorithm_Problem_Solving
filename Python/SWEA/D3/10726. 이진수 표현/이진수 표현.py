1b2238d83ebf10792f0eeb1a9c269401df5f5f81
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-03-07 10:38:43 +0900

    [D3] Title: 이진수 표현, Time: 588 ms, Memory: 62,720 KB -BaekjoonHub

--- Python/SWEA/D3/10726. 이진수 표현/이진수 표현.py
+++ Python/SWEA/D3/10726. 이진수 표현/이진수 표현.py
@@ -2,7 +2,8 @@ T = int(input())
 
 for tc in range(1, T+1):
     N, M = map(int, input().split())
-    if M & ((1 << N) - 1) == (1 << N) - 1:
+    result = ''
+    if M % (2**N) == 2**N - 1:
         result = 'ON'
     else:
         result = 'OFF'
