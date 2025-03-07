555dbb5f9f32a187a3297135b450766778707c46
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-03-07 10:37:07 +0900

    [D3] Title: 이진수 표현, Time: 585 ms, Memory: 62,080 KB -BaekjoonHub

--- Python/SWEA/D3/10726. 이진수 표현/이진수 표현.py
+++ Python/SWEA/D3/10726. 이진수 표현/이진수 표현.py
@@ -1,12 +1,9 @@
-def solution():
-    target = M
-    for _ in range(N):
-        if target & 1 == 0:
-            return "OFF"
-        target >>= 1
-    return "ON"
-
 T = int(input())
+
 for tc in range(1, T+1):
     N, M = map(int, input().split())
-    print(f'#{tc} {solution()}')
+    if M & ((1 << N) - 1) == (1 << N) - 1:
+        result = 'ON'
+    else:
+        result = 'OFF'
+    print(f'#{tc} {result}')
