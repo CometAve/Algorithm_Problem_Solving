ed77fa43dce2a685c6de8f344c293488af75c8dd
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-03-07 10:32:37 +0900

    [D3] Title: 이진수 표현, Time: 607 ms, Memory: 63,744 KB -BaekjoonHub

--- Python/SWEA/D3/10726. 이진수 표현/이진수 표현.py
+++ Python/SWEA/D3/10726. 이진수 표현/이진수 표현.py
@@ -1,16 +1,12 @@
-def dec_to_bin(n):
-    if n == 0:
-        return ""
-    return dec_to_bin(n // 2) + str(n % 2)
-
+def solution():
+    target = M
+    for _ in range(N):
+        if target & 1 == 0:
+            return "OFF"
+        target >>= 1
+    return "ON"
 
 T = int(input())
-
 for tc in range(1, T+1):
     N, M = map(int, input().split())
-    result = ''
-    if dec_to_bin(M)[-N:] == '1'*N:
-        result = 'ON'
-    else:
-        result = 'OFF'
-    print(f'#{tc} {result}')
+    print(f'#{tc} {solution()}')
