58e59742e788a2bf0cdf5a97ad5f4524763d2d0f
Comet Ave. <74771093+HyeseongRo@users.noreply.github.com>
2025-03-15 20:01:19 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1832 ms, Memory: 123160 KB -BaekjoonHub

---
+++ Python/백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -0,0 +1,39 @@
+# 문제 해결 방법
+# dp를 이용하여 팰린드롬인지 아닌지를 확인한다.
+# dp[i][j] = i부터 j까지의 부분 수열이 팰린드롬인지 아닌지를 저장한다.
+# 길이가 1인 부분 수열은 항상 팰린드롬이다.
+# 길이가 2인 부분 수열은 두 수가 같으면 팰린드롬이다.
+# 길이가 3 이상인 부분 수열은 양 끝의 수가 같고, 그 사이의 부분 수열이 팰린드롬이면 팰린드롬이다.
+# S와 E가 주어졌을 때, dp[S-1][E-1]을 출력하면 된다.
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+M = int(input())
+
+dp = [[0] * N for _ in range(N)]
+
+# 길이 1인 부분 수열은 항상 팰린드롬
+for i in range(N):
+    dp[i][i] = 1
+
+# 길이 2인 부분 수열 확인
+for i in range(N-1):
+    if arr[i] == arr[i+1]:
+        dp[i][i+1] = 1
+
+# 길이 3 이상인 부분 수열 확인
+for length in range(3, N+1):
+    for start in range(N-length+1): # 시작점
+        end = start + length - 1 # 끝점
+        if arr[start] == arr[end] and dp[start+1][end-1]: # 양 끝의 수가 같고, 그 사이의 부분 수열이 팰린드롬이면 팰린드롬
+            dp[start][end] = 1
+
+answer = []
+for _ in range(M):
+    S, E = map(int, input().split())
+    answer.append(str(dp[S-1][E-1]))
+
+sys.stdout.write('\n'.join(answer))
