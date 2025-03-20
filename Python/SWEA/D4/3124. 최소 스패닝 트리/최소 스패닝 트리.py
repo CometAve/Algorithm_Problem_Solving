2c186687930fe882b2293d0525ba3ad8bb310664
CometAve <74771093+CometAve@users.noreply.github.com>
2025-03-20 17:17:55 +0900

    [D4] Title: 최소 스패닝 트리, Time: 8,495 ms, Memory: 125,804 KB -BaekjoonHub

--- Python/SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.py
+++ Python/SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,38 +1,48 @@
-def find_parent(parent, x):
-    while parent[x] != x:
-        parent[x] = parent[parent[x]]
-        x = parent[x]
-    return x
+# GPT가 개선시킨 Prim 알고리즘 코드
 
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    if a == b:
-        return
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
+import heapq
+
+def prim(V, adj):
+    INF = float('inf')
+    key = [INF] * (V + 1)
+    mst = [False] * (V + 1)
+    key[1] = 0
+    hq = [(0, 1)]
+    result = 0
+    count = 0
+
+    while hq and count < V:
+        k, u = heapq.heappop(hq)
+        
+        # 이미 처리된 정점은 건너뛰기
+        if mst[u]:
+            continue
+            
+        mst[u] = True
+        result += k
+        count += 1
+
+        for v, w in adj[u]:
+            # 가지치기: 이미 MST에 포함된 정점은 처리하지 않음
+            if not mst[v] and w < key[v]:
+                key[v] = w
+                heapq.heappush(hq, (w, v))
+                
+    # 모든 정점이 MST에 포함되었는지 확인 (그래프가 연결되지 않은 경우)
+    if count < V:
+        return -1  # 또는 문제에 맞는 값 반환
+        
+    return result
 
 T = int(input())
 for tc in range(1, T + 1):
-    v, e = map(int, input().split())
-    edges = []
-    for _ in range(e):
-        a, b, cost = map(int, input().split())
-        edges.append((cost, a, b))
+    V, E = map(int, input().split())
+    adj = [[] for _ in range(V + 1)]
     
-    edges.sort(key=lambda x: x[0])
-    parents = [i for i in range(v + 1)]
-    
-    cnt = 0
-    result = 0
-    for cost, a, b in edges:
-        if find_parent(parents, a) != find_parent(parents, b):
-            union_parent(parents, a, b)
-            result += cost
-            cnt += 1
-        if cnt == v - 1:
-            break
-
+    for _ in range(E):
+        u, v, w = map(int, input().split())
+        adj[u].append((v, w))
+        adj[v].append((u, w))
+        
+    result = prim(V, adj)
     print(f'#{tc} {result}')
