c08f256c375a008d0b924c13dfda5c23f9244f45
CometAve <74771093+CometAve@users.noreply.github.com>
2025-03-21 10:54:16 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 873 ms, Memory: 60,544 KB -BaekjoonHub

--- Python/SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
+++ Python/SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
@@ -1,34 +1,45 @@
-# 문제 해결 방법
-# 1. N개의 식재료를 N/2개씩 나누는 모든 경우의 수를 구한다.
-# 2. 각각의 경우의 수에 대해 식재료들을 나누어 두 개의 요리를 만들어 맛의 차이를 계산한다.(절대값)
-# 3. 맛의 차이가 최소인 경우를 찾아 출력한다.
-
-T = int(input())
-
-for tc in range(1, T+1):
-    N = int(input())
-    synergy = [list(map(int, input().split())) for _ in range(N)]
-    # 최소 차이 값을 저장할 변수
-    min_diff = 10000
-    # 식재료들을 나누는 모든 경우의 수를 구한다.
-    for i in range(1 << N): # 비트 연산을 사용하여 모든 경우의 수를 구한다.
-        # A, B에 식재료들을 나누어 저장한다.
+# 감사합니다 지선생 전 단순한 오류를 못 찾고 있었네요. AI최고
+def solution():
+    T = int(input())
+    for tc in range(1, T+1):
+        N = int(input())
+        synergy = [list(map(int, input().split())) for _ in range(N)]
+        min_diff = float('inf')
         A, B = [], []
-        for j in range(N): # 식재료들을 나누어 저장한다.
-            if i & (1 << j): # 비트 연산을 사용하여 식재료들을 나눈다.
-                A.append(j)
-            else:
-                B.append(j)
-        # 식재료들을 나누어 두 개의 요리를 만들어 맛의 차이를 계산한다.
-        taste_A, taste_B = 0, 0 # A, B의 맛을 저장할 변수
-        # A, B의 맛을 계산한다.
-        for a in A:
-            for b in A:
-                taste_A += synergy[a][b] # A의 맛을 계산한다.
-        for a in B:
-            for b in B:
-                taste_B += synergy[a][b] # B의 맛을 계산한다.
-        # 맛의 차이가 최소인 경우를 찾아 저장한다.
-        diff = abs(taste_A - taste_B) # 절대값 계산
-        min_diff = min(min_diff, diff)
-    print(f'#{tc} {min_diff}')
+
+        def divide(idx):
+            nonlocal min_diff
+            if idx == N:
+                if len(A) == N//2 and len(B) == N//2:
+                    taste_A = 0
+                    taste_B = 0
+                    for i in A:
+                        for j in A:
+                            taste_A += synergy[i][j]
+                    for i in B:
+                        for j in B:
+                            taste_B += synergy[i][j]
+                    min_diff = min(min_diff, abs(taste_A - taste_B))
+                return
+
+            # 가지치기 A, B의 개수가 N/2가 되면 return
+            if len(A) > N//2 or len(B) > N//2:
+                return
+            
+            # A가 아직 덜 찼다면 A에 추가
+            if len(A) < N//2:
+                A.append(idx)
+                divide(idx+1)
+                A.pop()
+
+            # B가 아직 덜 찼다면 B에 추가
+            if len(B) < N//2:
+                B.append(idx)
+                divide(idx+1)
+                B.pop()
+        
+        divide(0)
+        print(f'#{tc} {min_diff}')
+
+if __name__ == '__main__':
+    solution()
