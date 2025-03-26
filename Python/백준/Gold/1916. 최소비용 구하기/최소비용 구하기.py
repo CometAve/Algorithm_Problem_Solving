2a940747bb84ad7f76720531c0fcb387795d90a8
CometAve <74771093+CometAve@users.noreply.github.com>
2025-03-26 17:17:51 +0900

    [Gold V] Title: 최소비용 구하기, Time: 172 ms, Memory: 51384 KB -BaekjoonHub

--- Python/백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ Python/백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,14 +1,16 @@
 import sys
 import heapq
 input = sys.stdin.readline
-print = sys.stdout.write
 
-def dijkstra(start, end, distance, graph):
-    q = []
-    heapq.heappush(q, (0, start))
+def dijkstra(start, end, N, graph):
+    distance = [float('inf')] * (N+1)
     distance[start] = 0
+    q = [(0, start)]
     while q:
         dist, now = heapq.heappop(q)
+        if now == end:
+            return dist
+        
         if distance[now] < dist:
             continue
         for i in graph[now]:
@@ -22,14 +24,19 @@ def solve():
     N = int(input())
     M = int(input())
     graph = [[] for _ in range(N+1)]
-    distance = [int(1e9)] * (N+1)
 
+    edge_dict = {}
     for _ in range(M):
         A, B, C = map(int, input().split())
+        if (A, B) not in edge_dict or C < edge_dict[(A, B)]:
+            edge_dict[(A, B)] = C
+
+    for (A, B), C in edge_dict.items():
         graph[A].append((B, C))
 
     start, end = map(int, input().split())
-    print(str(dijkstra(start, end, distance, graph)))
+    result = dijkstra(start, end, N, graph)
+    sys.stdout.write(str(result))
 
 if __name__ == "__main__":
     solve()
